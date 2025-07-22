dcf59ed85d3f20b1766d16739d4470fcc218d4d6
rajveeerr <rajveergreets@gmail.com>
2025-07-23 05:12:24 +0530

    feat: Fixed deployment error by cleaning up faulty unused component

--- web/src/pages/HomePage.tsx
+++ web/src/pages/HomePage.tsx
@@ -6,7 +6,6 @@ export const HomePage = () => {
   return (
     <>
       <HeroSection />
-      {/* <CategoryShowcase/> */}
       <FeaturesSection />
       {/* <BentoGridSection/> */}
       {/* <UseCaseSection/> */}
