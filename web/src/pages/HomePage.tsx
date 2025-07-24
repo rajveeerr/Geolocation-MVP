81abf987c56d1e53f621f5550b564dc27e1c59ce
rajveeerr <rajveergreets@gmail.com>
2025-07-24 19:59:37 +0530

    refactor: removed unused components from homepage and fixed tailwind config issues

--- web/src/pages/HomePage.tsx
+++ web/src/pages/HomePage.tsx
@@ -1,16 +1,16 @@
 import { HeroSection } from '@/components/landing/HeroSection';
 import { FeaturesSection } from '@/components/landing/FeaturesSection';
 import { MerchantCtaSection } from '@/components/landing/MerchantCtaSection';
-import { BentoGridSection } from '@/components/landing/BentoGridSection';
-import { UseCaseSection } from '@/components/landing/UseCaseSection';
+// import { BentoGridSection } from '@/components/landing/BentoGridSection';
+// import { UseCaseSection } from '@/components/landing/UseCaseSection';
 
 export const HomePage = () => {
   return (
     <>
       <HeroSection />
       <FeaturesSection />
-      <BentoGridSection/>
-      <UseCaseSection/>
+      {/* <BentoGridSection/> */}
+      {/* <UseCaseSection/> */}
       <MerchantCtaSection />
     </>
   );
