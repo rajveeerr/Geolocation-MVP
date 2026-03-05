2b91444f4cde104fe671085804916c87512b6577
rajveeerr <rajveergreets@gmail.com>
2026-03-05 21:35:57 +0530

    chore: remove React Query Devtools from main application render for cleaner production build

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App.tsx';
 import './styles/global.css';
 
@@ -11,7 +10,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 );
